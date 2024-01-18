cba54df1cbfcd2c149e77f508bacb93beb9f22f5
Boy753hub <boy753123@gmail.com>
2024-01-18 19:49:34 +0400

    created database and added folders in front for work

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+      MongooseModule.forRoot(
+        'mongodb+srv://tskhomelidzel:9nSkwx86J3WFpHDl@cluster0.kddojdd.mongodb.net/?retryWrites=true&w=majority',
+      )
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
